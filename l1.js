f2a08586ae5e538e6afc64f998707c2d85cd6cd1
No1Noseme <157268042+No1Noseme@users.noreply.github.com>
2024-04-02 17:26:20 +0530

    Add files via upload
    keep this file at same place , no need to make a special folder for this

---
+++ l1.js
@@ -0,0 +1,10 @@
+function expandSidebar() {
+    document.querySelector('.sidebar').style.height = '450px';
+    document.querySelector('.sidebar-options').style.display = 'block';
+}
+
+function collapseSidebar() {
+    document.querySelector('.sidebar').style.height = '46px';
+    document.querySelector('.sidebar-options').style.display = 'none';
+}
+
